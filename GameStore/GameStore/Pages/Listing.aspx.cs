92ac74e810245f32342bb8face6d981f0ef41485
Артем Асадов <kekc33rus@gmail.com>
2020-07-06 18:55:52 +0300

    [Dev] Отображение страницы товаров. Добавление разбиения на страницы

--- GameStore/GameStore/Pages/Listing.aspx.cs
+++ GameStore/GameStore/Pages/Listing.aspx.cs
@@ -12,8 +12,43 @@ namespace GameStore.Pages
     public partial class Listing : System.Web.UI.Page
     {
         private Repository repository = new Repository();
+
+        //Cколько товаров должно отображаться на странице
+        private int pageSize = 4;
+
+        /*
+        int.Parse пробует получить число из его строкового представления, в случае успеха возвращает число, иначе же бросает FormatException
+        int.TryParse проверяет, можно ли получить число из строки. Если это возможно - возвращает true
+        
+            Коллекция QueryString получает значения переменных их HTTP-строки запроса
+            directory-lookup.asp?name=fred
+            Добро пожаловать, <%= Request.QueryString("name") %>.
+        */
+        protected int CurrentPage
+        {
+            get
+            {
+                int page;
+                page = int.TryParse(Request.QueryString["page"], out page) ? page : 1;
+                return page > MaxPage ? MaxPage : page;
+            }
+        }
+
+        // Новое свойство, возвращающее наибольший номер допустимой страницы
+        //Math.Ceiling Возвращает наименьшее целое число, которое больше или равно заданному десятичному числу
+        protected int MaxPage
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)repository.Games.Count() / pageSize);
+            }
+        }
+
         protected IEnumerable<Game> GetGames() {
-            return repository.Games;
+            return repository.Games
+                .OrderBy(g=>g.GameId)
+                .Skip((CurrentPage-1)*pageSize)
+                .Take(pageSize);
         }
 
         protected void Page_Load(object sender, EventArgs e)
